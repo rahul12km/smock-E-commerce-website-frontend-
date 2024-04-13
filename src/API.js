4af17e20bc45a803d9834d6961228b7282f37dab
rahul12km <bestrahul123r@gmail.com>
2024-04-13 11:14:14 +0530

    fixed react hot toast

--- src/API.js
+++ src/API.js
@@ -1,5 +1,1 @@
-export const backendAPI = "https://smock-e-commerce-website-backend-seven.vercel.app";
-<<<<<<< HEAD
-    
-=======
->>>>>>> 012b979f5715bbba11301392b1e7cdddb0e79ad5
+export const backendAPI = "https://smock-e-commerce-website-backend-seven.vercel.app"
